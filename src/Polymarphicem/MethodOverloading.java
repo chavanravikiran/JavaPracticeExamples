3f8343e2fdda1e57a4a3c6027e8588d00a274d23
RaviKiran <ravikiranchavan97@gmail.com>
2025-02-19 23:46:11 +0530

    add Polymorphism question

---
+++ src/Polymarphicem/MethodOverloading.java
@@ -0,0 +1,26 @@
+package Polymarphicem;
+
+public class MethodOverloading {
+	// Method with two int parameters
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method with three int parameters
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Method with double parameters
+    double add(double a, double b) {
+        return a + b;
+    }
+    
+    public static void main(String[] args) {
+    	MethodOverloading math = new MethodOverloading();
+        System.out.println(math.add(5, 10));      // Calls method with two int parameters
+        System.out.println(math.add(5, 10, 15));  // Calls method with three int parameters
+        System.out.println(math.add(5.5, 10.5));  // Calls method with double parameters
+    }
+}
+
