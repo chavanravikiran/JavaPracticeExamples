ac2f68c308e95b7c2fbf933b6d64de3ec54a7be1
RaviKiran <ravikiranchavan97@gmail.com>
2025-03-16 18:39:11 +0530

    hashmap Compare using equals(),keySet(),and HashSet using value

--- src/Collection_HashMap/HashMapComparison.java
+++ src/Collection_HashMap/HashMapComparison.java
@@ -1,8 +1,12 @@
 package Collection_HashMap;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class HashMapComparison {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         // Creating two HashMaps
         HashMap<Integer, String> map1 = new HashMap<>();
@@ -25,5 +29,17 @@ public class HashMapComparison {
         // Comparing using equals()
         System.out.println("Are both HashMaps equal? " + map1.equals(map2)); // Output: true
         System.out.println("Are both HashMaps equal? " + map1.equals(map3)); // Output: false
+        
+        // Comparing using Key
+        System.out.println("Are both HashMaps equal using KeySet ? " + map1.keySet().equals(map2.keySet())); // Output: true
+        System.out.println("Are both HashMaps equal using KeySet ? " + map1.keySet().equals(map3.keySet())); // Output: false
+        
+     // Comparing using value
+       HashSet<String> map1Set = new HashSet<>(map1.values());
+       HashSet<String> map2Set = new HashSet<>(map2.values());
+       HashSet<String> map3Set = new HashSet<>(map3.values());
+       
+       System.out.println("Are both HashMaps equal using value ? "+map1Set.equals(map2Set));	//true
+       System.out.println("Are both HashMaps equal using value ? "+map1Set.equals(map3Set));	//false
     }
 }
