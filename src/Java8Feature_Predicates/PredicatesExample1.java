3484af0f3dfd5f90b2c40d7f2c212ca484e6df3f
RaviKiran <ravikiranchavan97@gmail.com>
2025-06-15 15:21:52 +0530

    add predicates and lambda expresion

--- src/Java8Feature_Predicates/PredicatesExample1.java
+++ src/Java8Feature_Predicates/PredicatesExample1.java
@@ -20,10 +20,28 @@ public class PredicatesExample1 {
 		
 		//2nd Way 
 		Predicate<String> startsWithRUsingCharAt = x->x.toLowerCase().charAt(0) == 'k';
-		System.out.println("If given Name Start with R -"+startsWithRUsingCharAt.test(name));
+		System.out.println("If given Name Start with K -"+startsWithRUsingCharAt.test(name));
 		
 		//End With 
 		Predicate<String> endWithRUsingCharAt =x -> x.toLowerCase().charAt(name.length()-1)=='n';
 		System.out.println("End With -"+endWithRUsingCharAt.test(name));
+		
+		//And default Method--Need both True
+		Predicate<String> andPredicates=startsWithRUsingCharAt.and(endWithRUsingCharAt);
+		System.out.println("'AND' - Start With K and End with N-" +andPredicates.test(name));//False
+		
+		Predicate<String> andPredicates1=startsWithR.and(endWithRUsingCharAt);
+		System.out.println("'AND' - Start With R and End with N-" +andPredicates1.test(name));//True
+		
+		//Or default Method--Need any one true
+		Predicate<String> orPredicates=startsWithRUsingCharAt.or(endWithRUsingCharAt);
+		System.out.println("'OR' - Start With K and End with N-" +orPredicates.test(name));//true
+		
+		Predicate<String> orPredicates1=startsWithR.or(endWithRUsingCharAt);
+		System.out.println("'OR' - Start With R and End with N-" +orPredicates1.test(name));//True
+		
+		//negate 
+		Predicate<String> negatePredicates1=startsWithR.negate();
+		System.out.println("'NEGATE' -" +negatePredicates1.test(name));//True asel tr false krte
 	}
 }
