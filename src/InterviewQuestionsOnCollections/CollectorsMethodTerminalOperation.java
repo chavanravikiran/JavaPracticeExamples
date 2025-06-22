d79ac3d32e3ba0242de4c7fa09f46a0a451542e6
RaviKiran <ravikiranchavan97@gmail.com>
2025-06-23 01:11:45 +0530

    add collectors class method

--- src/InterviewQuestionsOnCollections/CollectorsMethodTerminalOperation.java
+++ src/InterviewQuestionsOnCollections/CollectorsMethodTerminalOperation.java
@@ -3,7 +3,7 @@ package InterviewQuestionsOnCollections;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.Set;import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class CollectorsMethodTerminalOperation {
@@ -29,7 +29,23 @@ public class CollectorsMethodTerminalOperation {
 				.collect(Collectors.toMap(x->x, x->x.length()));
 		System.out.println(toMap);
 		
+		//toMap but duplicate key
+		//to Work with how to avoid collison while working with map
+		Map<Integer,String> toMap1= list.stream()
+				.collect(Collectors.toMap(x->x.length(), x->x,(oldValue,newValue)->oldValue +" : "+newValue));
+		System.out.println(toMap1);
+		
 		//groupingBy
 		
+		
+		//count from stream class and counting from Collectors class
+		Long count=list.stream().count();
+		Long counting=list.stream().collect(Collectors.counting());
+		System.out.println("Count using stream class : "+count +" Counting from Collectors class : "+counting);
+		
+		
+		//Joining
+		System.out.println(list.stream().collect(Collectors.joining(",")));
+		System.out.println(list.stream().collect(Collectors.joining(",", "!", "@")));
 	}
 }
