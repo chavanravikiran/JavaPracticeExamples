cc9e2a44fe622d1a6c793923de26244698ca1c44
RaviKiran <ravikiranchavan97@gmail.com>
2025-07-03 13:09:39 +0530

    add ArrayList Example

--- src/Collections_ArrayList/ArrayListExample1.java
+++ src/Collections_ArrayList/ArrayListExample1.java
@@ -22,14 +22,19 @@ public class ArrayListExample1 {
         // Accessing an element
         System.out.println("First Name: " + names.get(0));
 
+        //add at given Possition
+        names.add(1,"ravikiran");
+
         // Removing an element
         names.remove("Atul");
         System.out.println("After removing Atul: " + names);
+
 
         // Iterating over the list
         System.out.println("Iterating over the list:");
         for (String name : names) {
             System.out.println(name);
         }
+        
     }
 }
