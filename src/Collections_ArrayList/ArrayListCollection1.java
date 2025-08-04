1ab1966a32e6aa1dc2567e110a82c34ed34cfd47
RaviKiran <ravikiranchavan97@gmail.com>
2025-08-04 22:52:13 +0530

    add array List Collection example

---
+++ src/Collections_ArrayList/ArrayListCollection1.java
@@ -0,0 +1,15 @@
+package Collections_ArrayList;
+
+//ArrayList is non-synchronized
+//ArrayList available in Java.util package
+//Implement from List Interface
+//Based on Array data Structure
+//ArrayList is resizable (Dyanimc Size)
+//Array is fix size but ArrayList is size grow /shring  at runtime
+//It Maintained Insertion Order
+//Random Access is Possible
+//It can store duplicate Element
+//Array List is Slow as compare to LinkedList
+public class ArrayListCollection1 {
+
+}
