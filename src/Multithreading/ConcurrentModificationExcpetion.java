d1caeb562d52a7ea9175249a4f9ed24482f550a3
RaviKiran <ravikiranchavan97@gmail.com>
2025-08-23 00:09:04 +0530

    add example of fail fast and fail safe iterator example

--- src/Multithreading/ConcurrentModificationExcpetion.java
+++ src/Multithreading/ConcurrentModificationExcpetion.java
@@ -1,20 +1,38 @@
 package Multithreading;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ConcurrentModificationExcpetion {
 	public static void main(String[] args) {
 		
+		failFastIterator();//Modification nahi karu shakat fail fast madhe--ConcurrentModificationException throw error.Works on ArrayList HashMap,HashSet,LinkedList
+		failSafeIterator();//Modification karu shakat fail safe madhe--CopyOnWriteArrayList,ConcurrentHashMap
+		
+	}
+	
+	private static void failFastIterator() {
+		List<String> list = new ArrayList<>();
+		list.add("A");
+		list.add("B");
+		
+		for(String s : list) {
+			list.add("C");// ConcurrentModificationException
+		}
+		System.out.println(list);
+	}
+
+	private static void failSafeIterator() {
 		List<String> list = new CopyOnWriteArrayList<>();
 		list.add("A");
 		list.add("B");
-
+		
 		for (String s : list) {
-		    list.add("C"); // No exception, iterates safely
+			list.add("C"); // No exception, iterates safely
 		}
 		System.out.println(list); // [A, B, C, C]
-		
 	}
+
 }
 
