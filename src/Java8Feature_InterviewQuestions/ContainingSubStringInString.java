d54c4e2629243e1587534309c5391d01474524f5
RaviKiran <ravikiranchavan97@gmail.com>
2025-09-29 01:33:30 +0530

    asked in HCL interview

--- src/Java8Feature_InterviewQuestions/ContainingSubStringInString.java
+++ src/Java8Feature_InterviewQuestions/ContainingSubStringInString.java
@@ -1,29 +1,29 @@
 package Java8Feature_InterviewQuestions;
 
-//Given subString is present or not in String return  
+// Given subString is present or not in String return
 public class ContainingSubStringInString {
-	public static void main(String[] args) {
-		
-		String originalString = "ILoveIndiaAndIndiaIsBestCountryInWorld";
-		String subString = "Best";
-		
-		usingTraditionalWay(originalString,subString);
-	}
+    public static void main(String[] args) {
 
-	private static void usingTraditionalWay(String originalString, String subString) {
-		long originalLength= originalString.length();
-		long subStringLength= subString.length();
-		
-		String temp = "";
-		 for (int i = 0; i <= originalLength - subStringLength; i++) {
-			for (int j = i; j < i + subStringLength; j++) {
-				temp += originalString.charAt(j);
-			}
-			if (temp.equals(subString)) {
-				System.out.println("Substring found at index " + i);
-				return;
-			}
-		}
-		 System.out.println("Substring not found!");
-	}
+        String originalString = "ILoveIndiaAndIndiaIsBestCountryInWorld";
+        String subString = "and";
+
+        usingTraditionalWay(originalString, subString);
+    }
+
+    private static void usingTraditionalWay(String originalString, String subString) {
+        int originalLength = originalString.length();
+        int subStringLength = subString.length();
+
+        for (int i = 0; i <= originalLength - subStringLength; i++) {
+            String temp = ""; // reset for each new start index
+            for (int j = i; j < i + subStringLength; j++) {
+                temp += originalString.charAt(j);
+            }
+            if (temp.equals(subString)) {
+                System.out.println("Substring found at index " + i);
+                return;
+            }
+        }
+        System.out.println("Substring not found!");
+    }
 }
