98593a8af23aa6ae407ab1545eb9659eb11e7392
RaviKiran <ravikiranchavan97@gmail.com>
2025-10-04 00:19:55 +0530

    add and modify code

--- src/Collection_InterviewQuestions/MissingNumberFinderInArrayList.java
+++ src/Collection_InterviewQuestions/MissingNumberFinderInArrayList.java
@@ -10,39 +10,39 @@ public class MissingNumberFinderInArrayList {
 		int[] numbers = new int[99]; // Array from 1 to 100 with one number missing
 
 		// Example: number 57 is missing
-		Integer missing = 57;
-		Integer index = 0;
-		Integer actualSum = 0;
+		Integer expectedSum = 100 * (100 + 1) / 2;
+
+		usingCollection(expectedSum,numbers);
 		
+		List<Integer> intList = Arrays.stream(numbers).boxed().collect(Collectors.toList());
+		usingStream(intList,expectedSum);
+	}
+
+	private static void usingCollection(Integer expectedSum,int []numbers) {
+		Integer missing = 57;
+		Integer actualSum=0;
+		Integer index = 0;
+		
+
 		for (int i = 1; i <= 100; i++) {
 			if (i != missing) {
 				numbers[index++] = i;
 				actualSum += i;
 			}
 		}
-		Integer expectedSum = 100 * (100 + 1) / 2;
-
-		List<Integer> intList = Arrays.stream(numbers).boxed().collect(Collectors.toList());
-		
-		usingCollection(intList,actualSum,expectedSum);
-		usingStream(intList,actualSum,expectedSum);
-
-	}
-
-	private static void usingCollection(List<Integer> intList,Integer actualSum,Integer expectedSum) {
 		Integer missingNumber = expectedSum - actualSum;
 		System.out.println("Missing number is: " + missingNumber);
 	}
 
-	private static void usingStream(List<Integer> intList, int actualSum, Integer expectedSum) {
+	private static void usingStream(List<Integer> intList, Integer expectedSum) {
 	    Integer streamSum = intList.stream()
                        .mapToInt(Integer::intValue)
                        .sum();
-	    Integer usingReduce = intList.stream()
-	    		.reduce(0, Integer::sum);
-//	    		 .reduce(0, (a, b) -> a + b);
-
 	    System.out.println("Missing number (using stream): " + (expectedSum - streamSum));
+
+	    Integer usingReduce = intList.stream()
+//	    		.reduce(0, Integer::sum);
+	    		 .reduce(0, (a, b) -> a + b);
 	    System.out.println("Missing number (using stream): " + (expectedSum - usingReduce));
 	}
 
