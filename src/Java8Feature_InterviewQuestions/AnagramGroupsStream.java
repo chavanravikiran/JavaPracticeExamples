f8b3e093b24013c75b3983fdda00808e3f13b054
RaviKiran <ravikiranchavan97@gmail.com>
2025-11-05 01:37:16 +0530

    add anage example using collection

--- src/Java8Feature_InterviewQuestions/AnagramGroupsStream.java
+++ src/Java8Feature_InterviewQuestions/AnagramGroupsStream.java
@@ -7,8 +7,12 @@ public class AnagramGroupsStream {
 
 	public static void main(String[] args) {
 		List<String> words = Arrays.asList("listen","silent","eat", "tea", "tan", "ate", "nat", "bat");
+		
 		List<List<String>> groups = groupAnagrams(words);
 		System.out.println(groups); 
+		
+		List<List<String>> traditionWay = groupAnagramsTraditionWay(words);
+        System.out.println(traditionWay);
 	}
 
 	public static List<List<String>> groupAnagrams(List<String> words) {
@@ -24,4 +28,29 @@ public class AnagramGroupsStream {
         );
     }
 
+	 public static List<List<String>> groupAnagramsTraditionWay(List<String> words) {
+	        // Map to hold sorted word as key and list of anagrams as value
+	        Map<String, List<String>> map = new HashMap<>();
+
+	        // Loop through each word
+	        for (String word : words) {
+	            // Convert word to character array and sort it
+	            char[] chars = word.toCharArray();
+	            Arrays.sort(chars);
+
+	            // Create a string from sorted characters
+	            String key = new String(chars);
+
+	            // Check if key exists in map, if not add new list
+	            if (!map.containsKey(key)) {
+	                map.put(key, new ArrayList<>());
+	            }
+
+	            // Add the word to the list for this key
+	            map.get(key).add(word);
+	        }
+
+	        // Return all grouped anagrams as a list of lists
+	        return new ArrayList<>(map.values());
+	    }
 }
