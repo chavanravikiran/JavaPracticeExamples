b92ae0490760ac83da2b116d52e20a3aae7d52e2
RaviKiran <ravikiranchavan97@gmail.com>
2025-11-21 13:26:10 +0530

    changes in old code

--- src/Java8Feature_InterviewQuestions/FirstRepeatedCharacter.java
+++ src/Java8Feature_InterviewQuestions/FirstRepeatedCharacter.java
@@ -1,14 +1,35 @@
 package Java8Feature_InterviewQuestions;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 //Find First Repeaded Charectors from given string
 public class FirstRepeatedCharacter {
 	public static void main(String[] args) {
-		String str="Hello World";
-		
-		System.out.println(Arrays.stream(str.split(""))
-		.filter(x->str.indexOf(x) != str.lastIndexOf(x))
-		.findFirst().get());
+		String str = "Hello World";
+
+		usingStreamWay(str);
+		usingTraditionalWay(str);
+
+	}
+
+	private static void usingStreamWay(String str) {
+		System.out.println(
+				Arrays.stream(str.split(""))
+				.filter(x -> str.indexOf(x) != str.lastIndexOf(x))
+				.findFirst().get());
+
+		System.out.println("Using Traditional Way ");
+	}
+
+	private static void usingTraditionalWay(String str) {
+		List<String> list = new ArrayList<>();
+		for (String s : str.split("")) {
+			if (str.indexOf(s) != str.lastIndexOf(s)) {
+				System.out.println(s);
+				break;
+			}
+		}
 	}
 }
