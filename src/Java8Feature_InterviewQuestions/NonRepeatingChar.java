32af788e9b3f0c3cebf5363224f255c5fdcdfc77
RaviKiran <ravikiranchavan97@gmail.com>
2026-01-03 22:10:36 +0530

    asked in pratiti Technology

--- src/Java8Feature_InterviewQuestions/NonRepeatingChar.java
+++ src/Java8Feature_InterviewQuestions/NonRepeatingChar.java
@@ -10,15 +10,21 @@ public class NonRepeatingChar {
 	public static void main(String[] args) {
 		String str = "TEST";
 		
+		
 		usingTraditionalWay(str);
 		usingStream(str);
 	}
 
 	private static void usingTraditionalWay(String str) {
+		System.out.println("Using Traditional Way ");
 		LinkedHashMap<Character,Integer> map = new LinkedHashMap<>();
 		for(char ob : str.toCharArray()) {
 			map.put(ob, map.getOrDefault(ob, 0) + 1);
 		}
+		  map.entrySet().stream()
+          .filter(e -> e.getValue() == 1)
+          .forEach(e -> System.out.print(e.getKey()));
+		  System.out.println();
 	}
 
 	private static void usingStream(String str) {
@@ -29,7 +35,7 @@ public class NonRepeatingChar {
 				.filter(e -> e.getValue() == 1)
 				.map(x->x.getKey())
 				.collect(Collectors.joining(""));
-		
-		System.out.println(result);
+		System.out.println("Using Stream ");
+		System.out.print(result);
 	}
 }
