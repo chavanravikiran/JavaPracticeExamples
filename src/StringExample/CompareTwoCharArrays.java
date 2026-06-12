a305fd6a86624e45605294fb024026a8f82d9cef
Ravikiran Chavan <varsha.khese@hindindustec.com>
2026-06-13 00:04:39 +0530

    compare two char Arrays

---
+++ src/StringExample/CompareTwoCharArrays.java
@@ -0,0 +1,13 @@
+package StringExample;
+
+//Compare two char arrays
+public class CompareTwoCharArrays {
+	public static void main(String[] args) {
+		char[] arr1 = {'J','a','v','a'};
+		char[] arr2 = {'J','a','v','a'};
+		
+		System.out.println(arr1.equals(arr2));
+		
+		System.out.println(java.util.Arrays.equals(arr1, arr2));
+	}
+}
