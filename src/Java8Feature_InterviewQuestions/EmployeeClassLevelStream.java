35f074f3a33f238ad046a3d5ecee855c94b6c7a8
Ravikiran Chavan <varsha.khese@hindindustec.com>
2026-06-29 18:00:59 +0530

    Stream Quetions in Entity

---
+++ src/Java8Feature_InterviewQuestions/EmployeeClassLevelStream.java
@@ -0,0 +1,142 @@
+package Java8Feature_InterviewQuestions;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EmployeeClassLevelStream {
+	public static void main(String[] args) {
+		List<EntityEmployee> employee = Arrays.asList(
+				new EntityEmployee(1, "Ravi", "IT", 100),
+				new EntityEmployee(2, "Rushi", "Computer", 100),
+				new EntityEmployee(3, "Dhampal", "HR", 200),
+				new EntityEmployee(4, "Rakesh", "Account", 150),
+				new EntityEmployee(5, "Raj", "ScrumMaster", 250)
+				);
+		
+		// 1. Print all employees
+		employee.stream()
+		.forEach(System.out::print);
+		
+		//2. Print Only Who has EmployeeName ="Ravi".
+		List<EntityEmployee> nameWith = employee.stream()
+		.filter(x->x.getName().equals("Ravi"))
+		.collect(Collectors.toList());
+		
+		System.out.println(nameWith);
+		
+		//3. print Only Employees List
+		List<String> employeeName = employee.stream()
+		.map(x->x.getName())
+		.collect(Collectors.toList());
+		
+		System.out.println(employeeName);
+		
+		
+		// 4. Employees whose salary > 150
+		List<EntityEmployee> greaterThanSalary = employee.stream()
+		.filter(x->x.getSalary()>=150)
+		.collect(Collectors.toList());
+		
+		System.out.println("Employees whose salary > 150 count is"+greaterThanSalary.size()+" - "+greaterThanSalary);
+		
+		// 5. Increase salary by 10%
+		List<EntityEmployee> incrementSalary = employee.stream()
+		.map(x->new EntityEmployee(x.getId(), x.getName(), x.getDepartment(), x.getSalary()*10))
+		.collect(Collectors.toList());
+		
+		System.out.println(" Increase salary by 10% "+incrementSalary);
+	
+		//6. Sort by salary in desc order
+		List<EntityEmployee> sortedSalary= employee.stream()
+		.sorted((a,b)->Double.compare(b.getSalary(), a.getSalary()))
+		.collect(Collectors.toList());
+		
+		System.out.println("Sort by salary in desc order "+ sortedSalary );
+		
+		//7.Sort by Salary and Name
+//		List<EntityEmployee> sortedSalaryAndName = employee.stream()
+//		.sorted((x,y)->Double.compare(x.getSalary(), y.getSalary()))
+//		.sorted((a,b)->a.getName().compareTo(b.getName()))
+//		.collect(Collectors.toList());
+		//Here Multiple sorted() calls are generally not recommended, because each call performs a complete sort.
+		
+		//For multiple sorting conditions, use Comparator.thenComparing(), which is more efficient and clearer.
+		List<EntityEmployee> sortedSalaryAndName = employee.stream()
+		.sorted(Comparator.comparing(EntityEmployee::getSalary).reversed().thenComparing(EntityEmployee::getName))
+		.collect(Collectors.toList());
+		System.out.println("Sort by Salary and Name " +sortedSalaryAndName);
+		
+		//7. Highest Salary Employee
+		EntityEmployee highestSalary = employee.stream()
+		.max((x,y)->Double.compare(x.getSalary(), y.getSalary()))
+		.get();
+		
+		System.out.println("Highest Salary Employee "+highestSalary);
+		
+		//8. Highest Salary Employee
+		System.out.println(employee.stream()
+		.min((x,y)->Double.compare((x.getSalary()), y.getSalary())));
+		
+	}
+}
+
+class EntityEmployee {
+
+	private int id;
+	private String name;
+	private String department;
+	private double salary;
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "EntityEmployee [id=" + id + ", name=" + name + ", department=" + department + ", salary=" + salary
+				+ "]";
+	}
+
+	public EntityEmployee(int id, String name, String department, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.department = department;
+		this.salary = salary;
+	}
+
+	public EntityEmployee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+}
