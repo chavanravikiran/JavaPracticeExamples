e5ab36b388909a1353cd5f1053d75d92dc0c431c
Ravikiran Chavan <varsha.khese@hindindustec.com>
2026-07-03 00:15:30 +0530

    Output Trace questions solve on OOPS

--- src/BasicQuestion/OutputTraceEx14.java
+++ src/BasicQuestion/OutputTraceEx14.java
@@ -2,10 +2,10 @@ package BasicQuestion;
 
 public class OutputTraceEx14 {
 	public static void main(String[] args) {
-		String str1 = "Ravikiran";
-		String str2 = "Ravikiran";
+		String str1 = "Ravikiran";//SCP
+		String str2 = "Ravikiran";//SCP
 		
-		String str3 = new String("Ravikiran");
+		String str3 = new String("Ravikiran");//Heap
 		
 		System.out.println(str1 == str2);//true
 		System.out.println(str1.equals(str2));//true
---
+++ src/BasicQuestion/OutputTraceEx17.java
@@ -0,0 +1,16 @@
+package BasicQuestion;
+
+public class OutputTraceEx17 {
+	static int data[];
+	public static void main(String[] args) {
+		System.out.println(data.length);
+	}
+}
+
+//main method chya aat madhe use karat asel variable tr te hi static pahije nahi tr error denar te
+//main chya baher asel tr initialization chi garaj naste pn local variable asel tr initilization karave lagte nahi tr compilation error denar
+//Collection default value is null
+
+//Output
+//------------------------------
+//Null pointer Exception
